5d418f7907228c45afd0b8a7270291bc31923d09
Matheus <mathbrlima2007@gmail.com>
2026-05-25 04:25:18 -0300

    Material do curso

---
+++ DESAFIOS/04.Boas práticas de programação/desafio4-4.js
@@ -0,0 +1,7 @@
+// 7. Peça ao usuário para inserir sua idade com prompt. Com base na idade inserida, utilize um if para verificar se a pessoa é maior ou menor de idade, exibindo uma mensagem apropriada no console.
+idade = Number(prompt("Insira sua idade: "));
+if (idade >= 18) {
+    console.log("Maior de idade");
+} else {
+    console.log("Menor de idade");
+}
---
+++ DESAFIOS/04.Boas práticas de programação/desafio5-4.js
@@ -0,0 +1,11 @@
+// 8. Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.
+
+numero = Number(prompt("Insira um valor: "));
+
+if (numero > 0) {
+    console.log("Positivo");
+} else if (numero < 0) {
+    console.log("Negativo")
+} else {
+    console.log("Zero")
+}
