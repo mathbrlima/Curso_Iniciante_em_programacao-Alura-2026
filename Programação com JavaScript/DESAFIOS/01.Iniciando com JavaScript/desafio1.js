91e3bb61474bdd5456860e4febe70b9466a2692a
Matheus <mathbrlima2007@gmail.com>
2026-05-26 04:03:33 -0300

    Material do curso

--- Programação com JavaScript/DESAFIOS/01.Iniciando com JavaScript/desafio1.js
+++ Programação com JavaScript/DESAFIOS/01.Iniciando com JavaScript/desafio1.js
@@ -12,4 +12,4 @@ let saldoDisponivel = 1000;
 alert("Erro! Preencha todos os campos");
 // 7. Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
 let mensagemDeErro = "Erro! Preencha todos os campos";
-alert(mensagemDeErro);
+alert(mensagemDeErro); 
